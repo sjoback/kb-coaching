20a6b204e34c64e621d80ce2dd27085fb5f93681
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2022-04-25 22:58:47 +0200

    env

--- pages/workouts/index.tsx
+++ pages/workouts/index.tsx
@@ -20,7 +20,8 @@ function Workouts({ data }) {
 }
 
 export async function getServerSideProps() {
-   let dev = process.env.NODE_ENV !== "production";
+   let dev = process.env.NODE_ENV == "development";
+
    let { DEV_URL, PROD_URL } = process.env;
 
    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/workouts`);
