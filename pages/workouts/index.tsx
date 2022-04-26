d121ac3fd925b342729c50d222392d972e181f7c
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2022-04-26 11:23:59 +0200

    deploy fixes

--- pages/workouts/index.tsx
+++ pages/workouts/index.tsx
@@ -21,29 +21,31 @@ function Workouts({ data }) {
 }
 
 export async function getServerSideProps() {
-   //    let { db } = await connectToDatabase();
+   let { db } = await connectToDatabase();
 
-   //    let workouts = await db
-   //       .collection("workouts")
-   //       .find({})
-   //       .sort({ added: 1 })
-   //       .toArray();
+   let workouts = await db
+      .collection("workouts")
+      .find({})
+      .sort({ added: 1 })
+      .toArray();
 
-   //    return {
-   //       props: {
-   //          data: JSON.parse(JSON.stringify(workouts)),
-   //       },
-   //    };
-
-   let dev = process.env.NODE_ENV == "development";
-   let { DEV_URL, PROD_URL } = process.env;
-   let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/workouts`);
-   let data = await response.json();
    return {
       props: {
-         data: data["message"],
+         data: JSON.parse(JSON.stringify(workouts)),
       },
    };
+
+   // let dev = process.env.NODE_ENV == "development";
+   // let { DEV_URL, PROD_URL } = process.env;
+   // let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/workouts`);
+   // let data = await response.json();
+   // console.log(data);
+
+   // return {
+   //    props: {
+   //       data: data["message"],
+   //    },
+   // };
 }
 
 export default Workouts;
