2d6a8ff41be4fea2baac3fa058730ffbda442267
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2022-04-26 10:08:34 +0200

    Update index.tsx

--- pages/workouts/index.tsx
+++ pages/workouts/index.tsx
@@ -2,11 +2,11 @@ import { connectToDatabase } from "../../lib/mongodb";
 import Button from "components/Button/Button";
 import List from "components/List/List";
 
-function Workouts({ data }) {
+function Workouts() {
    return (
       <div>
          <h1>Workouts</h1>
-         <List linkType="workouts" items={data} />
+         {/* <List linkType="workouts" items={data} />
 
          <div className="link-list-button">
             <Button
@@ -15,7 +15,7 @@ function Workouts({ data }) {
                component="link"
                link="/workouts/add"
             />
-         </div>
+         </div> */}
       </div>
    );
 }
@@ -23,12 +23,8 @@ function Workouts({ data }) {
 // export async function getServerSideProps() {
 //    let dev = process.env.NODE_ENV == "development";
 //    let { DEV_URL, PROD_URL } = process.env;
-//    console.log(":::: get server side props ::::");
-
 //    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/workouts`);
-
 //    let data = await response.json();
-
 //    return {
 //       props: {
 //          data: data["message"],
@@ -36,30 +32,20 @@ function Workouts({ data }) {
 //    };
 // }
 
-export async function getStaticProps() {
-   const { db } = await connectToDatabase();
-   const workouts = await db
-      .collection("workouts")
-      .find({})
-      .sort({ added: -1 })
-      .limit(1000)
-      .toArray();
-   return {
-      props: {
-         data: JSON.parse(JSON.stringify(workouts)),
-      },
-   };
+// export async function getStaticProps() {
+//    const { db } = await connectToDatabase();
+//    const workouts = await db
+//       .collection("workouts")
+//       .find({})
+//       .sort({ added: -1 })
+//       .limit(1000)
+//       .toArray();
+//    return {
+//       props: {
+//          data: JSON.parse(JSON.stringify(workouts)),
+//       },
+//    };
 
-   // let dev = process.env.NODE_ENV == "development";
-   // let { DEV_URL, PROD_URL } = process.env;
-
-   // let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/workouts`);
-   // let data = await response.json();
-   // return {
-   //    props: {
-   //       data: data["message"],
-   //    },
-   // };
-}
+// }
 
 export default Workouts;
