134041a710684523e384129d626febff3e9a126a
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2022-04-26 10:11:28 +0200

    Update index.tsx

--- pages/workouts/index.tsx
+++ pages/workouts/index.tsx
@@ -2,11 +2,11 @@ import { connectToDatabase } from "../../lib/mongodb";
 import Button from "components/Button/Button";
 import List from "components/List/List";
 
-function Workouts() {
+function Workouts({ data }) {
    return (
       <div>
          <h1>Workouts</h1>
-         {/* <List linkType="workouts" items={data} />
+         <List linkType="workouts" items={data} />
 
          <div className="link-list-button">
             <Button
@@ -15,22 +15,22 @@ function Workouts() {
                component="link"
                link="/workouts/add"
             />
-         </div> */}
+         </div>
       </div>
    );
 }
 
-// export async function getServerSideProps() {
-//    let dev = process.env.NODE_ENV == "development";
-//    let { DEV_URL, PROD_URL } = process.env;
-//    let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/workouts`);
-//    let data = await response.json();
-//    return {
-//       props: {
-//          data: data["message"],
-//       },
-//    };
-// }
+export async function getServerSideProps() {
+   let dev = process.env.NODE_ENV == "development";
+   let { DEV_URL, PROD_URL } = process.env;
+   let response = await fetch(`${dev ? DEV_URL : PROD_URL}/api/workouts`);
+   let data = await response.json();
+   return {
+      props: {
+         data: data["message"],
+      },
+   };
+}
 
 // export async function getStaticProps() {
 //    const { db } = await connectToDatabase();
