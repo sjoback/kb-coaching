82793ef543c4945f78747515680cb522ac7a78fa
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2022-05-03 10:00:28 +0200

    add mobile nav

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,10 +1,26 @@
 import styles from "./Navigation.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBurger } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import classnames from "classnames";
 
 function Navigation() {
+   const [mobileOpen, setMobileOpen] = useState(false);
+
+   function toggleOpen() {
+      if (mobileOpen) setMobileOpen(false);
+      else setMobileOpen(true);
+   }
+
    return (
-      <nav className={styles.wrapper}>
+      <nav
+         className={classnames(
+            styles.wrapper,
+            mobileOpen ? styles.mobileOpen : ""
+         )}
+      >
          <Link href="/">
             <a className={styles.navLogo}>
                <Image layout="fill" src="/logo.webp" />
@@ -15,6 +31,13 @@ function Navigation() {
          <Link href="/warmups">Warmups</Link>
          <Link href="/mitts">Mitts</Link>
          <Link href="/physicals">Physicals</Link>
+
+         <div onClick={toggleOpen} className={styles.navToggle}>
+            <FontAwesomeIcon
+               icon={faBurger}
+               style={{ fontSize: 32, color: "#fff" }}
+            />
+         </div>
       </nav>
    );
 }
